fca039fde4aa9de09689d561b4b645f0c834d2bc
Konsta O <konstaduck@gmail.com>
2021-04-07 13:10:45 +0300

    Added check for expired logins

--- bot.py
+++ bot.py
@@ -13,11 +13,15 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
-# Authentication and trusted users
-trusted_users = np.loadtxt('trusted.txt', dtype=str)
-superuser = YOUR_OWN_TG_ID
+# Superuser is admin of the bot
+superuser = ADD_YOUR_OWN_TG_ID
+bot_token = ADD_YOUR_BOT_TOKEN
 logged_in = False
 
+# Return value constants for the login verification
+LOGIN_FOUND     = 0
+LOGIN_NOT_FOUND = 1
+LOGIN_EXPIRED   = 2
 
 # Define a few command handlers. These usually take the two arguments update and context.
 def start(update: Update, _: CallbackContext) -> None:
@@ -39,7 +43,7 @@ def login(update: Update, _: CallbackContext) -> None:
     if logged_in:
         update.message.reply_text('Olet jo sisällä...')
     else:
-        if user.username in trusted_users:
+        if trustedfile_contains_name(user.username):
             logged_in = True
             update.message.reply_text(f'Tervetuloa, {user.first_name}!')
         else:
@@ -53,20 +57,25 @@ def logout(update: Update, _: CallbackContext) -> None:
         logged_in = False
         update.message.reply_text('Uloskirjautuminen onnistui.')
     else:
-        update.message.reply_text('Yritä ensin kirjautua sisään ;)')    
+        update.message.reply_text('Yritä ensin kirjautua sisään ;) /login')    
     
 def default(update: Update, _: CallbackContext) -> None:
     update.message.reply_text('Hyväksyn vain komentoja. /help')
 
 def photo(update: Update, _: CallbackContext) -> None:
-    if logged_in:
+    global logged_in
+    status = verify_login_status(update.effective_user.username)
+    
+    if status == LOGIN_FOUND:
         update.message.reply_photo(photo=open('test.png','rb'))
+    elif status == LOGIN_EXPIRED:
+        logged_in = False
+        update.message.reply_text('Sinut on poistettu luotettujen listalta. Kirjaudutaan ulos..')
     else:
-        update.message.reply_text('Vain sisäänkirjautuneille..')
+        update.message.reply_text('Vain sisäänkirjautuneille.. /login')
         
-def addtrusted(update: Update, context: CallbackContext) -> None:
+def add_trusted(update: Update, context: CallbackContext) -> None:
     user = update.effective_user
-    global trusted_users
     
     # Allow superuser to add person to the trusted list
     if user.id == superuser:
@@ -74,24 +83,22 @@ def addtrusted(update: Update, context: CallbackContext) -> None:
             update.message.reply_text('Kenet pitäisi lisätä?')
         else:
             to_be_added = context.args[0]
-            if to_be_added in trusted_users:
+            if trustedfile_contains_name(to_be_added):
                 update.message.reply_text('Käyttäjä löytyy jo listalta.')
             else:
                 with open("trusted.txt", "a+") as file_object:
                     file_object.seek(0)
                     file_object.write("\n")
                     file_object.write(to_be_added)
-                trusted_users = np.append(trusted_users, to_be_added)
                 update.message.reply_text(f'Lisättiin @{to_be_added} listalle.')   
-        
     elif logged_in:
         update.message.reply_text('Vain ylläpitäjällä on oikeus tähän komentoon.')
     else:
         update.message.reply_text('Tämä toiminnallisuus ei ole yleisessä käytössä.')
         
-def removetrusted(update: Update, context: CallbackContext) -> None:
+def remove_trusted(update: Update, context: CallbackContext) -> None:
     user = update.effective_user
-    global trusted_users
+    global logged_in
     
     # Allow superuser to delete trusted person from list
     if user.id == superuser:
@@ -99,14 +106,7 @@ def removetrusted(update: Update, context: CallbackContext) -> None:
             update.message.reply_text('Kenet pitäisi poistaa?')
         else:
             to_be_deleted = context.args[0]
-            found = False
-        
-            with open('trusted.txt', 'r') as file_in:
-                for line in file_in:
-                    if line.strip() == to_be_deleted.strip():
-                        found = True
-            
-            if not found:
+            if not trustedfile_contains_name(to_be_deleted):
                 update.message.reply_text(f'Käyttäjänimeä @{to_be_deleted} ei löytynyt listalta.')   
             else:
                 os.rename('trusted.txt','trusted_old.txt')
@@ -115,20 +115,21 @@ def removetrusted(update: Update, context: CallbackContext) -> None:
                         if not to_be_deleted.strip() in line.strip():
                             new.write(line)
                 os.remove('trusted_old.txt')
-                trusted_users = np.delete(trusted_users, np.argwhere(trusted_users==to_be_deleted))
                 update.message.reply_text(f'Käyttäjänimi @{to_be_deleted} poistettiin listalta.')
-
     elif logged_in:
         update.message.reply_text('Vain ylläpitäjällä on oikeus tähän komentoon.')
     else:
-        update.message.reply_text('Tämä toiminnallisuus ei ole yleisessä käytössä.')        
+        update.message.reply_text('Tämä toiminnallisuus ei ole yleisessä käytössä.')    
         
    
         
 def trusted(update: Update, context: CallbackContext) -> None:
     user = update.effective_user
+    global logged_in
+    
     if user.id == superuser:
         # List all trusted persons
+        trusted_users = np.loadtxt('trusted.txt', dtype=str)
         msg = 'Luotetut henkilöt:\n'
         for person in trusted_users.ravel():
             msg = msg + "@" + person + "\n"
@@ -137,11 +138,30 @@ def trusted(update: Update, context: CallbackContext) -> None:
         update.message.reply_text('Vain ylläpitäjällä on oikeus tähän komentoon.')
     else:
         update.message.reply_text('Tämä toiminnallisuus ei ole yleisessä käytössä.')
+            
+        
+def verify_login_status(user: str):
+    if logged_in:
+        if trustedfile_contains_name(user):
+            return LOGIN_FOUND
+        else:
+            return LOGIN_EXPIRED
+    else:
+        return LOGIN_NOT_FOUND
+        
+def trustedfile_contains_name(user: str):
+    found = False
+    with open('trusted.txt', 'r') as file_in:
+            for line in file_in:
+                if line.strip() == user.strip():
+                    found = True
+    return found
+
 
 def main() -> None:
 
     # Create the Updater and pass it your bot's token.
-    updater = Updater(ADD_YOUR_BOT_TOKEN)
+    updater = Updater(bot_token)
 
     # Get the dispatcher to register handlers
     dispatcher = updater.dispatcher
@@ -152,9 +172,9 @@ def main() -> None:
     dispatcher.add_handler(CommandHandler("login", login))
     dispatcher.add_handler(CommandHandler("logout", logout))
     dispatcher.add_handler(CommandHandler("photo", photo))
-    dispatcher.add_handler(CommandHandler("addtrusted", addtrusted))
-    dispatcher.add_handler(CommandHandler("removetrusted", removetrusted))
-    dispatcher.add_handler(CommandHandler("deletetrusted", removetrusted))
+    dispatcher.add_handler(CommandHandler("addtrusted", add_trusted))
+    dispatcher.add_handler(CommandHandler("removetrusted", remove_trusted))
+    dispatcher.add_handler(CommandHandler("deletetrusted", remove_trusted))
     dispatcher.add_handler(CommandHandler("trusted", trusted))
     
     dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, default))
